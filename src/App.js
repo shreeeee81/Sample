9281836d9852c7e8a37cd34282b46d2df8e7ce2a
shreeeee81 <shreeeee81@gmail.com>
2021-11-09 14:28:35 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import BottomNavigation from './Components/BottomNavigation';
+function App() {
+  return (
+    <div className="App">
+      <BottomNavigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BottomNavigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class BottomNavigation extends Component {
+    const [value, setValue];
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
